b096cf571bf0759fbe26279f0eb60503fe3fb627
Tomoya Usami <tousami@zlab.co.jp>
2020-04-03 10:30:03 +0900

    Migrate to UpstreamAuthority plugin

---
+++ cmd/server/vault-upstream-authority/main.go
@@ -0,0 +1,8 @@
+/**
+ * Copyright 2020, Z Lab Corporation. All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package main
